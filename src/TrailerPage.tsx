b820dc8ac99474cc003213403ad0b0a23e7ae0b1
KokiOta <kokiota1998@gmail.com>
2022-07-17 07:22:01 +0000

    fix

--- src/TrailerPage.tsx
+++ src/TrailerPage.tsx
@@ -4,6 +4,7 @@ import Button from "@suid/material/Button";
 import IconButton from "@suid/material/IconButton";
 import Grid from "@suid/material/Grid"
 import Box from "@suid/material/Box";
+import CircularProgress from '@suid/material/CircularProgress';
 
 import PlayArrowIcon from '@suid/icons-material/PlayArrow';
 import PauseIcon from '@suid/icons-material/Pause';
@@ -11,6 +12,7 @@ import PauseIcon from '@suid/icons-material/Pause';
 import YouTubePlayer from 'youtube-player';
 import PlayerStates from 'youtube-player/dist/constants/PlayerStates';
 
+import './TrailerPage.css';
 
 const TrailerPage = () => {
 	
@@ -20,6 +22,7 @@ const TrailerPage = () => {
 	const [player, setPlayer] = createSignal<ReturnType<typeof YouTubePlayer> | null>(null);
 	const [playing, setPlaying] = createSignal(false);
 	const [filmarkLink, setFilmarkLink] = createSignal('https://filmarks.com/');
+	const [loading, setLoading] = createSignal(true);
 
 	const createPlayer = (videoId: string) => {
 		const player = YouTubePlayer('player', {
@@ -41,21 +44,25 @@ const TrailerPage = () => {
 		});
 
 		setPlayer(player);
+		setLoading(false);
 	}
 
 	createEffect(() => {
 		createPlayer("89U5Sva2qdc");
 	})
 	
+	const sleep = (msec: number) => new Promise(resolve => setTimeout(resolve, msec));
 	const nextTrailer = () => {
 		player()?.destroy();
 		setPlaying(false);
+		setLoading(true);
 		
 		getTrailerUrl()
-			.then((res) => {
+			.then(async (res) => {
 				setFilmarkLink(res.film[0].filmarks);
 				const id = res.film[0].link.match(/.*v=(.*)/)[1];
 				createPlayer(id);
+				await sleep(1000);
 				player()?.playVideo();
 			});
 	}
@@ -78,30 +85,39 @@ const TrailerPage = () => {
 				}}
 			>
 
-				<Grid container height="100vh" justifyContent="center" alignItems="center">
+				<Grid container height="90vh" justifyContent="center" alignItems="center">
 					<Grid container item xs={12} md={12} justifyContent="center">
 						<div id='player' style={{width: '80%', height: "80vh"}}></div>
 					</Grid>
 
-					<Grid container item xs={4} md={4} justifyContent="center" alignItems="center">
-						<Button variant="outlined" onClick={() => {window.open(filmarkLink(), '_blank');}}>Filmarks</Button>
-					</Grid>
-					<Grid container item xs={4} md={4} justifyContent="center" alignItems="center">
-						<IconButton color="primary" onClick={handlePlayButton}>
-							<Switch fallback={<PlayArrowIcon fontSize="large"/>}>
-								<Match when={playing()}>
-									<PauseIcon fontSize="large"/>
-								</Match>
-								<Match when={!playing()}>
-									<PlayArrowIcon fontSize="large"/>
-								</Match>
-							</Switch>
-						</IconButton>
-						
-					</Grid>
-					<Grid container item xs={4} md={4} justifyContent="center" alignItems="center">
-						<Button variant="outlined" onClick={nextTrailer}>Next</Button>
-					</Grid>
+					<Switch fallback={
+						<Grid container item xs={12} md={12} justifyContent="center">
+							<CircularProgress />
+						</Grid>
+					}>
+						<Match when={!loading()}>
+
+							<Grid container item xs={4} md={4} justifyContent="center" alignItems="center">
+								<Button variant="outlined" onClick={() => {window.open(filmarkLink(), '_blank');}} size="large">Filmarks</Button>
+							</Grid>
+
+							<Grid container item xs={4} md={4} justifyContent="center" alignItems="center">
+								<IconButton color="primary" onClick={handlePlayButton}>
+									<Switch fallback={<PlayArrowIcon fontSize='large'/>}>
+										<Match when={playing()}>
+											<PauseIcon fontSize='large'/>
+										</Match>
+									</Switch>
+								</IconButton>
+							</Grid>
+							
+							<Grid container item xs={4} md={4} justifyContent="center" alignItems="center">
+								<Button variant="outlined" onClick={nextTrailer} size="large">Next</Button>
+							</Grid>
+
+						</Match>
+					</Switch>
+
 				</Grid>
 
 			</Box>
